6c29f257a28235a06d79b70477dda564d490e25a
Pvrock786 <thepvrock76@gmail.com>
2020-04-07 21:24:27 +0530

    react update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+ import ScrollAnimation from 'react-animate-on-scroll';
 import {SectionsContainer, Section} from 'react-fullpage';
+import "animate.css/animate.min.css";
 import home from './assets/image/home.jpg'
 import html from './assets/image/html.png'
 import css from './assets/image/css.png'
@@ -32,6 +34,7 @@ class App extends Component {
 			
 		});
 	};
+
 	backdropClickHandler = () => {
 		this.setState({ sideDrawerOpen: false });
 	};
@@ -41,123 +44,142 @@ class App extends Component {
 			backdrop = <Backdrop click={this.backdropClickHandler} />;
 		}
 		console.log(this.state.sideDrawerOpen)
-		let options = {
-			sectionClassName:     'section',
-			anchors:              ['sectionOne', 'sectionTwo', 'sectionThree' , 'sectionFour'],
-			scrollBar:           false,
-			navigation:          false,
-			verticalAlign:       false,
-			arrowNavigation:      false
-		  };
+
         return ( 
             <div className="App">
 				
-	<SectionsContainer {...options}>
-    <Section>
+	{/* <SectionsContainer {...options}> */}
+    {/* <Section> */}
 		   {/* //////////////////// HOME/////////////////// */}
-		   <div className="home">
-	
+		   <div className="home" id= "home">
+		  
 	<div className="header">
 				  <h3 className="">My Portfolio</h3>
 		  <nav className="navbar">
 			  <ul>
-				  <li><a href="#sectionOne">Home</a></li>
-				  <li><a href="#sectionTwo">Skill</a></li>
-				  <li><a href="#sectionThree">About</a></li>
+				  <li><a href="#home">Home</a></li>
+				  <li><a href="#skills">Skills</a></li>
+				  <li><a href="#about">About</a></li>
+				  <li><a href="#education">Education</a></li>
 				  
 			  </ul>
 			  
 		  </nav>	  
 	</div>
 	<div className="toggle_button">
-	<DrawerToggleButton click ={this.drawerToggleClickHandler}/>
+	<DrawerToggleButton click ={this.drawerToggleClickHandler} id="button"/>
 	</div>
 			  <SideDrawer show = {this.state.sideDrawerOpen}  click ={this.drawerToggleClickHandler}/>
 			  {backdrop}
-		  <h2 className="heading">WEB DEVELOPMENT </h2>
-		 
+			  <ScrollAnimation animateIn="fadeInDown" duration = {3}  animateOnce={true}>
+		  <h2 className="name-heading">Praveen  Shukla </h2>
+		  <h4 className="quoto"> I'm here to to create beautiful design , that makes diffrence</h4>
+		  </ScrollAnimation>
+		  <ScrollAnimation animateIn="fadeInLeft" duration = {3}  animateOnce={true} >
 	  <div className="social-link">
 			  <a href ="https://github.com/Pvrock786"> <FaGithub/></a>
 			  <a href ="http://praveen.shukla.77312477@fb.com"> <FaFacebook/></a>
 			  <a href ="https://www.instagram.com/pv_shukla/"> <FaInstagram/></a>
 			  <a href ="https://in.linkedin.com/in/praveen-shukla-3522b6162"> <FaLinkedinIn/></a>
 		  </div>
-		  <a className="btn" href="#sectionTwo">&#8964;</a>
-  
+		  </ScrollAnimation>
+		  
+			
+		  <a className="btn" href="#skills">&#8964;</a>
+		
+	
     </div>
-	    </Section>
-    <Section>
-		{/* ///////////////// SkILLS///////////////////// */}
-  <div className="skills">
-		 
-			<h2 className="sk-head">MY SKILLS</h2>
+		{/* ****************************************  SKILLS   ********************************************* */}
+    <div className="skills" id="skills">	 
+			<h3 className="heading">MY SKILLS</h3>
 			<hr/>
 			<div className="interest">
-				<img src= {html} alt="" />
-        <img src= {css} alt="" />
-        <img src= {js} alt="" />
-        <img src= {angular} alt="" />
-        <img src= {react} alt="" />
-       
-        {/* <img src= {mongo} alt="" /> */}
-        {/* <img src= {photoshop} alt="" /> */}
-		<img src= {mongo} alt="" />
-		
-        <img src= {firebase} alt="" />
-        {/* <img src= {api} alt="" /> */}
-        <img src= {word} alt="" />
-        
-        <img src= {node} alt="" />
-        <img src= {git} alt="" />
-       
+			<ScrollAnimation animateIn="zoomIn" duration = {3}  animateOnce={true}>
+				 <img src= {html} alt = ""   className= "img"/>
+			</ScrollAnimation>
+			<ScrollAnimation animateIn="zoomIn" duration = {3}  animateOnce={true}>
+                 <img src= {css} alt = ""   className= "img"/>
+		    </ScrollAnimation>
+			<ScrollAnimation animateIn="zoomIn" duration = {3}  animateOnce={true}>
+			     <img src= {js} alt = ""   className= "img"/>
+			</ScrollAnimation>
+			<ScrollAnimation animateIn="zoomIn" duration = {3}  animateOnce={true}>
+			     <img src= {angular} alt = ""   className= "img"/>
+			</ScrollAnimation>
+			<ScrollAnimation animateIn="zoomIn" duration = {3}  animateOnce={true} className="scroll">
+			     <img src= {react} alt = ""   className= "img"/>
+			</ScrollAnimation>
+			<ScrollAnimation animateIn="zoomIn" duration = {3}  animateOnce={true}>
+                 <img src= {photoshop} alt = ""   className= "img"/>
+		     </ScrollAnimation>
+			<ScrollAnimation animateIn="zoomIn" duration = {3}  animateOnce={true}>
+			     <img src= {mongo} alt = ""   className= "img"/>
+			</ScrollAnimation>
+			<ScrollAnimation animateIn="zoomIn" duration = {3}  animateOnce={true}>
+			     <img src= {firebase} alt = ""   className= "img"/>
+			</ScrollAnimation>
+			<ScrollAnimation animateIn="zoomIn" duration = {3}  animateOnce={true}>
+			     <img src= {word} alt = ""   className= "img"/>
+			</ScrollAnimation>
+			<ScrollAnimation animateIn="zoomIn" duration = {3}  animateOnce={true}>
+			     <img src= {git} alt = ""   className= "img"/>
+			</ScrollAnimation>
+			<ScrollAnimation animateIn="zoomIn" duration = {3}  animateOnce={true}>
+			     <img src= {node} alt = ""   className= "img"/>
+			</ScrollAnimation>
+			<ScrollAnimation animateIn="zoomIn" duration = {3}  animateOnce={true}>
+			     <img src= {api} alt = ""   className= "img"/>
+			</ScrollAnimation>
 			</div>
-		</div>
-		</Section>
-		{/* //////////////////////// Skills #2 //////////////////////////// */}
-		{/* <Section>
-		
-  <div className="skills">
-		 
-			<h2 className="sk-head">MY SKILLS</h2>
-			<hr/>
-			<div className="interest">
-			
-			
-			</div>
-		</div>
-		</Section>*/}
-    <Section> 
-		{/* ///////////////////// ABOUT ///////////////////////// */}
-
-<div className="about">
-		  <h2 className="text-center me-about">ABOUT ME</h2>
+	</div>
+		{/* ****************************************ABOUT****************************************************** */}
+<div className="about" id="about">
+		  <h2 className="heading">ABOUT ME</h2>
 		  <hr/>
 	<div className="about-me">
-		  <img src={profile} />
+		<div>
+		<ScrollAnimation animateIn="fadeInLeft" duration= {2}  animateOnce={true} delay = {500}>
+		  <img src={profile} className="img"/>
+		  </ScrollAnimation>
+		  </div>
 	<div className="me">
+	<ScrollAnimation animateIn="fadeInRight" duration= {2}  animateOnce={true}>
 		  <h2 className="hello">HELLO, I'M PRAVEEN SHUKLA</h2>
-		  <p className="me-desc">I'm a self taught web designer, developer. I'm currently part of a small web development team in an upcoming start-up, building web and mobile applications. My passion is to use technology based solutions, to help solve real world challenges. Competences: Languages and Frameworks: Javascript, HTML5, CSS3, jQuery, Bootstrap3, Angular js, React js,JavaScript. Tools & expertise: Git, Responsive Web Design, Agile Methodologies.</p>
-	</div>
+		  <p className="me-desc">I'm a self taught web designer, developer. I'm currently part of a small web 
+		  development team in an upcoming start-up, building web and mobile applications. My passion is to use 
+		  technology based solutions, to help solve real world challenges. Competences: Languages and Frameworks:
+		   Javascript, HTML5, CSS3, jQuery, Bootstrap3, Angular js, React js,JavaScript. Tools & expertise: Git, 
+		   Responsive Web Design, Agile Methodologies.</p>
+		  </ScrollAnimation>
+		  	</div>
 	</div>
 	  </div>
-	  </Section>
-	  </SectionsContainer>
-		{/* //////////////////////// CONTACT /////////////////////////// */}
-   {/* <div className="contact" id="contact">
-	
-			<h2 className="c-hd">CONTACT US</h2>
-			<hr/>
-      <div className="get-touch">
-			<input type="text" className="input" placeholder="Name"/>
-			<input type="text" className="input" placeholder="E-mail"/>
-			<textarea  className="textarea" placeholder="Message"></textarea>
-		<br/>
-    	<input type="submit" name="submit" value="Send" className="submit"/>
-		</div>
-	</div> */}
-	
-   
-  
+          {/* ***********************************Education***************************************************** */}
+		  <div className="education" id="education">
+			   <h3 className = "heading edu"> EDUCATION</h3>
+			    <hr/>
+				<ScrollAnimation animateIn='fadeInLeft' duration = {3} animateOnce={true}>
+                 <div className="card one">
+					 <h5> Bachelor of Engineering (B.E), Computer Science</h5> 
+					  <p> SLIET, Longowal, Punjab (Deemed University) </p>
+					  <p> Year Of Starting: 2017</p>
+				 </div>
+				 </ScrollAnimation>
+				 <ScrollAnimation animateIn='fadeInLeft' duration = {3}  delay={1000} animateOnce={true}>
+				 <div className="card two">
+					 <h5> XII (Senior Secondary), Science</h5> 
+					  <p> CBSE Board (Brij Behari Sahai Vidya Mandir Kadilpur)    </p>
+					  <p> Year of Completion: 2016</p>
+				 </div>
+				 </ScrollAnimation>
+				 <ScrollAnimation animateIn='fadeInLeft' duration = {2}  delay={1300} animateOnce={true}>
+				 <div className="card three">
+					 <h5> X (Secondary) , Science</h5> 
+					  <p> CSBE Board (Brij Behari Sahai Vidya Mandir Kadilpur)</p>
+					  <p> Year of Completion: 2014</p>
+				 </div>
+				 </ScrollAnimation>
+		  </div>
     </div>
          );
     }
--- src/component/SideDrawer/SideDrawer.js
+++ src/component/SideDrawer/SideDrawer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './SideDrawer.css'
-
+import { FaGithub,FaFacebook ,FaInstagram,FaLinkedinIn} from 'react-icons/fa';
 const SideDrawer = props => {
     let drawerClasses = 'side-drawer';
     if (props.show) {
@@ -11,11 +11,19 @@ const SideDrawer = props => {
     return (
       <nav className={drawerClasses}>
         <ul>
-        <li><a href="#sectionOne" onClick={props.click} >Home</a></li>
-				  <li><a href="#sectionTwo" onClick={props.click}>Skill</a></li>
-				  <li><a href="#sectionThree" onClick={props.click}>About</a></li>
-				  <li><a href="#contact" onClick={props.click}> Contact</a></li>
+        <li><a href="#home" onClick={props.click} >Home</a></li>
+				  <li><a href="#skills" onClick={props.click}>Skill</a></li>
+				  <li><a href="#about" onClick={props.click}>About</a></li>
+          <li><a href="#education" onClick={props.click}>Education</a></li>
+        
+          <div className="social-link-mobile">
+          <a href ="https://github.com/Pvrock786"> <FaGithub/></a>
+			  <a href ="http://praveen.shukla.77312477@fb.com"> <FaFacebook/></a>
+			  <a href ="https://www.instagram.com/pv_shukla/"> <FaInstagram/></a>
+			  <a href ="https://in.linkedin.com/in/praveen-shukla-3522b6162"> <FaLinkedinIn/></a>
+		  </div>
         </ul>
+       
       </nav>
     );
   };
